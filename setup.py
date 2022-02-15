00149039b24cadc5670ee1d99c75cb99bca176ae
danfergo <danfergo@gmail.com>
2022-02-15 22:54:23 +0000

    adding include_package_data to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
-
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
 
 setup(
     name='yarok',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    version='0.0.3',
+    include_package_data=True,
+    version='0.0.4',
     description='YAROK - Yet another robot framework',
     author='danfergo',
     entry_points={
