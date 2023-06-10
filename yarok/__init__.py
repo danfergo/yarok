682399715d403ac0dd03f7f0223239317d6d25b4
Daniel Fernandes Gomes <danfergo@gmail.com>
2023-06-10 20:48:13 +0000

    saving changes in preparation for version 0.0.36

--- yarok/__init__.py
+++ yarok/__init__.py
@@ -10,7 +10,7 @@ from yarok.platforms.mjc.platform import PlatformMJC
 from yarok.platforms.hw.platform import PlatformHW
 
 __PLATFORM__ = None
-__VERSION__ = '0.0.35'
+__VERSION__ = '0.0.36'
 
 
 def wait(fn=None, cb=None):
