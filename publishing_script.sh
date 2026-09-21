767a576d2cba8f8d30951116795e7e0c53cf3af0
Daniel Fernandes Gomes <danfergo@gmail.com>
2026-09-21 15:53:35 +0100

    saving changes in preparation for version 0.0.44

--- publishing_script.sh
+++ publishing_script.sh
@@ -7,7 +7,7 @@ rm -rf build && rm -rf dist
 rm -rf yarok/comm/components/ur5e/ikfastpy
 
 
-PATCH=43
+PATCH=44
 VERSION="0.0."$PATCH
 
 PREV_PATCH=
@@ -15,8 +15,8 @@ PREV_VERSION="0.0."$(($PATCH - 1))
 echo "Publishing Yarok=="$VERSION
 
 sed -i "s+[0-9]\.[0-9]\.[0-9][0-9]+$VERSION+g" yarok/__init__.py
-sed -i "s+[0-9]\.[0-9]\.[0-9][0-9]+$PREV_VERSION+g" setup.py
-sed -i "s+[0-9]\.[0-9]\.[0-9][0-9]+$PREV_VERSION+g" pyproject.toml
+# sed -i "s+[0-9]\.[0-9]\.[0-9][0-9]+$PREV_VERSION+g" setup.py
+sed -i "s+[0-9]\.[0-9]\.[0-9][0-9]+$VERSION+g" pyproject.toml
 
 # re-export environment.yaml
 conda env export | head -n -1 > environment.yaml
@@ -29,7 +29,6 @@ bumpversion --current-version $PREV_VERSION patch setup.py
 
 git add -A
 git commit -m "updating version to "$VERSION
-
 
 echo "--- BUILD ---"
 rm -rf dist build *.egg-info
